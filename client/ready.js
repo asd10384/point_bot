56cc4ab3dae61f4703ea1584eab21acaae963f7a
asd10384 <tmdgks0466@naver.com>
2021-05-08 23:42:59 +0900

    activity 수정

--- client/ready.js
+++ client/ready.js
@@ -9,30 +9,37 @@ module.exports = async function (client = new Client) {
     client.on('ready', async () => {
         var db_text = '';
         try {
-            var db_key_list = Object.keys(db.all()[0]['data']);
-            for (i in db_key_list) {
-                db_text += `\n       \`${i+1}. ${db_key_list[i]} \``;
-                for (j=0; j<Object.keys(db.all()[0]['data'][db_key_list[i]]).length; j++) {
-                    var db_user_id = Object.keys(db.all()[0]['data'][db_key_list[i]])[j];
-                    db_text += `\n       ${db_user_id}　:　`;
-                    db_text += `${Object.values(db.all()[0]['data'][db_key_list[i]])[j]}\n`;
+            var w = db.fetch(`db`);
+            for (i in w) {
+                db_text += `\n${i}: {\n`;
+                for (j in w[i]) {
+                    if (String(w[i][j]) === '[object Object]') {
+                        db_text += `    ${j}: {\n`;
+                    } else {
+                        db_text += `    ${j}: ${w[i][j]}\n`;
+                    }
+                    for (k in w[i][j]) {
+                        db_text += `        ${k}: ${w[i][j][k]}\n`;
+                    }
+                    if (String(w[i][j]) === '[object Object]') db_text += `    }\n`;
                 }
+                db_text += `}\n`;
             }
         } catch(err) {
-            db_text = '\n       없음\n';
+            db_text = '\n없음\n';
         }
         console.log(`
-    
-            =========================
-              이름 : ${client.user.username}
-            
-              태그 : ${client.user.tag}
-            ==========================
-    
+
+===============================
+ 이름 : ${client.user.username}
+
+ 태그 : ${client.user.tag}
+===============================
+
         `);
-        console.log('       ====================');
+        console.log('===============================');
         console.log(db_text);
-        console.log('       ====================\n');
+        console.log('===============================\n');
 
         client.user.setPresence({
             activity: {
@@ -42,32 +49,23 @@ module.exports = async function (client = new Client) {
             status: 'online'
         });
         
-        activity(client, [
-            {
-                name: `${dfprefix}help`,
-                type: 'WATCHING',
-                time: 10000,
-            },
-            {
-                name: `${dfprefix}도움말`,
-                type: 'WATCHING', // PLAYING
-                time: 10000,
-            },
-        ]);
+        activity(client);
     });
 }
 
-function activity(client = new Client, list = Array) {
+function activity(client = new Client) {
     var i = 0;
-    setInterval(() => {
+    var list = eval(process.env.activity);
+    setInterval(function() {
+        list = eval(process.env.activity);
         client.user.setPresence({
             activity: {
                 name: list[i].name,
                 type: list[i].type,
             },
-            status: 'online',
+            status: list[i].status,
         });
         i++;
         if (i >= list.length) i = 0;
-    }, list[i].time);
+    }, list[i].time * 1000);
 }
