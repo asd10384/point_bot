000db255117670808c849d7c6c9b11f9d60093d0
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-05-16 17:04:17 +0900

    오류수정

--- log/log.js
+++ log/log.js
@@ -20,14 +20,14 @@ async function botlog(message = new Message, text = '', date = new Date) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(date);
     text = `${message.guild.name} 서버\n${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${text}\n`;
     console.log(text);
-    appendFileSync(`log/${path}.txt`, text, {encoding: 'utf8'});
+    appendFileSync(`log/bot.txt`, text, {encoding: 'utf8'});
 }
 
 async function quizlog(message = new Message, text = '', date = new Date) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(date);
     text = `${message.guild.name} 서버\n${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${text}`;
     console.log(text);
-    appendFileSync(`log/${path}.txt`, text, {encoding: 'utf8'});
+    appendFileSync(`log/quiz.txt`, text, {encoding: 'utf8'});
 }
 
 async function sitelog(text = '', date = new Date) {
