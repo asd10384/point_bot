f43aa945c9d25965d563a63f7ac91902502ebd1a
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-05-17 20:56:49 +0900

    오류수정

--- log/log.js
+++ log/log.js
@@ -9,6 +9,7 @@ module.exports = {
     quizlog,
     sitelog,
     errlog,
+    selfchecklog,
 };
 
 async function readylog(text = '', date = new Date()) {
@@ -19,35 +20,35 @@ async function readylog(text = '', date = new Date()) {
 }
 async function botlog(message = new Message, text = '', date = new Date) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(date);
-    text = `${message.guild.name} 서버\n${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${text}\n\n`;
+    text = `${message.guild.name} 서버\n${year}년${month}월${day}일 ${week}요일 ${mds.az(hour, 2)}:${mds.az(min, 2)}:${mds.az(sec, 2)}\n${text}\n\n`;
     console.log(text);
     appendFileSync(`log/bot.txt`, text, {encoding: 'utf8'});
 }
 
 async function quizlog(message = new Message, text = '', date = new Date) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(date);
-    text = `${message.guild.name} 서버\n${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${text}\n`;
+    text = `${message.guild.name} 서버\n${year}년${month}월${day}일 ${week}요일 ${mds.az(hour, 2)}:${mds.az(min, 2)}:${mds.az(sec, 2)}\n${text}\n`;
     console.log(text);
     appendFileSync(`log/quiz.txt`, text, {encoding: 'utf8'});
 }
 
 async function sitelog(text = '', date = new Date) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(date);
-    text = `${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${text}\n\n`;
+    text = `${year}년${month}월${day}일 ${week}요일 ${mds.az(hour, 2)}:${mds.az(min, 2)}:${mds.az(sec, 2)}\n${text}\n\n`;
     console.log(text);
     appendFileSync(`log/site.txt`, text, {encoding: 'utf8'});
 }
 
 async function selfchecklog(text = '', date = new Date) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(date);
-    text = `${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${text}\n\n`;
+    text = `${year}년${month}월${day}일 ${week}요일 ${mds.az(hour, 2)}:${mds.az(min, 2)}:${mds.az(sec, 2)}\n${text}\n\n`;
     console.log(text);
     appendFileSync(`log/selfcheck.txt`, text, {encoding: 'utf8'});
 }
 
 async function errlog(err) {
     var { year, month, day, week, hour, min, sec } = mds.format.nowdate(new Date);
-    text = `${year}년${month}월${day}일 ${week}요일 ${mds.az(hour)}:${mds.az(min)}:${mds.az(sec)}\n${err}\n\n`;
+    text = `${year}년${month}월${day}일 ${week}요일 ${mds.az(hour, 2)}:${mds.az(min, 2)}:${mds.az(sec, 2)}\n${err}\n\n`;
     console.log(err);
     appendFileSync(`log/err.txt`, text, {encoding: 'utf8'});
 }
